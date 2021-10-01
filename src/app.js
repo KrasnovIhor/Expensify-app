f5a40d111feef7d569ee89b5eeb3a19155f6d951
Ihor Krasnov <krasnov@bramblingtech.com>
2021-10-01 15:28:18 +0300

    modified webpack.config

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 import { Provider } from "react-redux";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
 const store = configureStore();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,50 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
-  },
+module.exports = (env) => {
+  const isProduction = env.production;
+  const mode = isProduction ? "production" : "development";
+  const CSSExtract = new MiniCssExtractPlugin({
+    filename: "[name].css",
+  });
+  const cssLoader = {
+    loader: "css-loader",
+    options: {
+      sourceMap: true,
+    },
+  };
+  const sassLoader = {
+    loader: "sass-loader",
+    options: {
+      sourceMap: true,
+    },
+  };
+
+  return {
+    mode,
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    plugins: [CSSExtract],
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/i,
+          use: [MiniCssExtractPlugin.loader, cssLoader, sassLoader],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
 };
